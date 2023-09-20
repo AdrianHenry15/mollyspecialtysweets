b55a94ed6f93a340c62fa2600482a23147e865c9
Adrian <adrianhenry2115@gmail.com>
2023-09-20 16:13:32 -0400

    .env

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,30 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import "./globals.css";
 
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register({
+    onUpdate: (registration) => {
+        alert("New content is available. Please refresh the page.")
+    },
+    onSuccess: (registration) => {
+        console.log("Content is cahced for offline use.")
+    }
+});
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.getRegistration().then(registration => {
+        if (registration) {
+            console.log('Service worker is running');
+        } else {
+            console.log('Service worker is not running');
+        }
+    });
+} else {
+    console.log('Service workers are not supported')
+}
