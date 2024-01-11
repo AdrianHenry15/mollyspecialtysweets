5b0782cd6a6e5bf4d10c184d750bfee8f1d317b6
Adrian <adrianhenry2115@gmail.com>
2024-01-11 07:18:05 -0500

    some fade-in stuff

--- components/layout/icons-row/icon-item.tsx
+++ components/layout/icons-row/icon-item.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Image from "next/image";
-import React, { useEffect, useRef } from "react";
+import React from "react";
 import Link from "next/link";
 
 import { ChevronRightIcon } from "@heroicons/react/24/outline";
@@ -15,31 +15,8 @@ interface IIconItemProps {
 }
 
 const IconItem = (props: IIconItemProps) => {
-    const containerRef = useRef<HTMLDivElement>(null);
-
-    useEffect(() => {
-        const options = {
-            threshold: 0.1, // Adjust the threshold as needed (percentage of element visibility)
-        };
-
-        const callback: IntersectionObserverCallback = (entries) => {
-            entries.forEach((entry) => {
-                if (entry.isIntersecting) {
-                    containerRef.current?.classList.add("show");
-                }
-            });
-        };
-
-        const observer = new IntersectionObserver(callback, options);
-
-        if (containerRef.current) {
-            observer.observe(containerRef.current);
-        }
-
-        return () => observer.disconnect(); // Cleanup observer on component unmount
-    }, []);
     return (
-        <article ref={containerRef} className={`fade-in flex flex-col px-4 py-10 justify-center items-center flex-1 md:mx-10`}>
+        <article className={`flex flex-col px-4 py-10 justify-center items-center flex-1 md:mx-10`}>
             <Image src={props.src} alt="cookie-icon" width={75} height={75} />
             <h5 className="text-white my-4 font-semibold text-2xl">{props.name}</h5>
             <p className="text-white font-light w-[300px]">{props.description}</p>
