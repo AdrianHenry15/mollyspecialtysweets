5cc2e40e4981804fa96e40a6ca7bb549e07c4322
Adrian <adrianhenry2115@gmail.com>
2024-01-18 15:27:45 -0500

    contactus

--- components/splash.tsx
+++ components/splash.tsx
@@ -47,7 +47,7 @@ const Splash = (props: ISplashProps) => {
                     <Image src={Logo} alt="logo" className="w-24 py-2" />
                     <h1 className="text-white text-3x1 md:text-5xl">{props.title}</h1>
                     <div className="my-4">
-                        <Link href={"/contact"} className="border bg-gray-300 text-black border-gray-300 py-2 px-5">
+                        <Link href={"/contact-us"} className="border bg-gray-300 text-black border-gray-300 py-2 px-5">
                             Contact Us
                         </Link>
                         <Link href={"/estimate"} className="border  text-white border-gray-300 py-2 px-5 ml-4">
