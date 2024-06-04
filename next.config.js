b99ae558db0c0be156d025f19e82e5bcbe15b5c1
Adrian <adrianhenry2115@gmail.com>
2024-06-04 15:43:01 -0400

    updates to env stuff

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    env: {
+        NEXT_PUBLIC_SERVICE_ID: process.env.NEXT_PUBLIC_SERVICE_ID,
+        NEXT_PUBLIC_TEMPLATE_ID: process.env.NEXT_PUBLIC_TEMPLATE_ID,
+        NEXT_PUBLIC_KEY: process.env.NEXT_PUBLIC_KEY,
+        NEXT_PRIVATE_KEY: process.env.NEXT_PRIVATE_KEY
+    },
     typescript: {
         ignoreBuildErrors: true,
     },
