ca6da94ea1d628459c07f53a9062ff67218652aa
Adrian <adrianhenry2115@gmail.com>
2024-07-16 20:22:30 -0400

    updates that fixed the db bug

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
-        DATABASE_URL: process.env.DATABASE_URL,
         NEXT_PUBLIC_SERVICE_ID: process.env.NEXT_PUBLIC_SERVICE_ID,
         NEXT_PUBLIC_TEMPLATE_ID: process.env.NEXT_PUBLIC_TEMPLATE_ID,
         NEXT_PUBLIC_KEY: process.env.NEXT_PUBLIC_KEY,
