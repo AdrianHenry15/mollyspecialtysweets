efc18924737b3859296551e90bdc36954463242f
Adrian <adrianhenry2115@gmail.com>
2024-08-25 11:41:36 -0400

    change

--- app/(root)/order/cakes/page.tsx
+++ app/(root)/order/cakes/page.tsx
@@ -3,14 +3,11 @@ import { Metadata } from "next";
 
 import CakeForm from "@/components/forms/cake";
 import PageFormContainer from "@/components/page-form-container";
-<<<<<<< HEAD
-=======
 
 export const metadata: Metadata = {
     title: "Create A Cake",
     description: "Create A Cake With Our Pre-Selected Frostings, Fillings, and Flavors",
 };
->>>>>>> 9335e79e39368709760c5fb21f15cf5c10373bec
 
 const CakePage = () => {
     return (
